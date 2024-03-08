6822dc12305d5c7cfd7820c42dd447387415c3f9
KIRAN AHMED <162012113+coading-git-hub@users.noreply.github.com>
2024-03-08 22:05:28 +0500

    its my second file

---
+++ quest 2/challange.js
@@ -0,0 +1,15 @@
+//Q # 1
+/*console.log(`Albert Einstein once said, "A person who never made a mistake never tried anything new."`);
+
+
+// Q # 2
+
+ let famous_person: string = "Albert Einstein";
+
+let message: string = `${famous_person} once said,"A person who never made a mistake never tried anything new."`;
+
+console.log(message);*/
+//Q # 3
+var name1 = "\t\nKIRAN \t\n";
+//console.log(name1);
+console.log(name1.trim());
---
+++ quest 2/challange.ts
@@ -0,0 +1,21 @@
+//Q # 1
+
+/*console.log(`Albert Einstein once said, "A person who never made a mistake never tried anything new."`);
+
+
+// Q # 2
+
+ let famous_person: string = "Albert Einstein";
+
+let message: string = `${famous_person} once said,"A person who never made a mistake never tried anything new."`;
+
+console.log(message);*/
+
+  //Q # 3
+
+  let name1: string = "\t\nKIRAN \t\n";
+
+  //console.log(name1);
+  console.log(name1.trim());
+
+
